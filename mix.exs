2b04ba7a908e40ec657ec81441b81fa8f2093791
Susumu Yamazaki <zacky1972@gmail.com>
2019-01-11 17:16:44 +0900

    add description and package

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule Garnet.MixProject do
       version: "0.0.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -24,4 +26,19 @@ defmodule Garnet.MixProject do
       { :ex_doc,      ">= 0.0.0", only: :dev}
     ]
   end
+
+
+  defp description() do
+    "Garnet: uses Hastega and Cockatoris"
+  end
+
+  defp package() do
+    [
+      name: "garnet",
+      maintainers: ["Susumu Yamazaki", "Masakazu Mori", "Yoshihiro Ueno", "Hideki Takase"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/zeam-vm/garnet"}
+    ]
+  end
+
 end
